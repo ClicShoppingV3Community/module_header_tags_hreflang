0c455d66cf57cf91d83e279e4b1de03ecbe7bf40
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:49 -0400

    reformat

--- includes/modules/header_tags/ht_hreflang.php
+++ includes/modules/header_tags/ht_hreflang.php
@@ -1,19 +1,20 @@
 <?php
-/**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   use ClicShopping\Sites\Shop\HeaderTags;
   use ClicShopping\OM\Registry;
   use ClicShopping\OM\CLICSHOPPING;
 
-  class ht_hreflang {
+  class ht_hreflang
+  {
     public $code;
     public $group;
     public $title;
@@ -21,35 +22,40 @@
     public $sort_order;
     public $enabled = false;
 
-    public function __construct() {
+    public function __construct()
+    {
       $this->code = get_class($this);
       $this->group = basename(__DIR__);
 
       $this->title = CLICSHOPPING::getDef('module_header_tags_hreflang_title');
       $this->description = CLICSHOPPING::getDef('module_header_tags_hreflang_description');
 
-      if ( defined('MODULE_HEADER_TAGS_HREFLANG_STATUS') ) {
+      if (defined('MODULE_HEADER_TAGS_HREFLANG_STATUS')) {
         $this->sort_order = MODULE_HEADER_TAGS_HREFLANG_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_TAGS_HREFLANG_STATUS == 'True');
       }
     }
 
-    public function execute() {
+    public function execute()
+    {
 
       $CLICSHOPPING_Template = Registry::get('Template');
 
-      $CLICSHOPPING_Template->addBlock('<link rel="alternate" hreflang="' . CLICSHOPPING::getDef('html_lang') . '" href="'. HeaderTags::getCanonicalUrl() .'">', $this->group);
+      $CLICSHOPPING_Template->addBlock('<link rel="alternate" hreflang="' . CLICSHOPPING::getDef('html_lang') . '" href="' . HeaderTags::getCanonicalUrl() . '">', $this->group);
     }
 
-    public function isEnabled() {
+    public function isEnabled()
+    {
       return $this->enabled;
     }
 
-    public function check() {
+    public function check()
+    {
       return defined('MODULE_HEADER_TAGS_HREFLANG_STATUS');
     }
 
-    public function install() {
+    public function install()
+    {
       $CLICSHOPPING_Db = Registry::get('Db');
 
       $CLICSHOPPING_Db->save('configuration', [
@@ -76,17 +82,19 @@
         ]
       );
 
-       return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
-                                               ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
-                            );
+      return $CLICSHOPPING_Db->save('configuration', ['configuration_value' => '1'],
+        ['configuration_key' => 'WEBSITE_MODULE_INSTALLED']
+      );
     }
 
-    public function remove() {
+    public function remove()
+    {
       return Registry::get('Db')->exec('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
     }
 
-    public function keys() {
-      return array('MODULE_HEADER_TAGS_HREFLANG_STATUS', 
-                   'MODULE_HEADER_TAGS_HREFLANG_SORT_ORDER');
+    public function keys()
+    {
+      return array('MODULE_HEADER_TAGS_HREFLANG_STATUS',
+        'MODULE_HEADER_TAGS_HREFLANG_SORT_ORDER');
     }
   }
